03464f5771a4a682c902f20a483a3916b8104949
Kasia Gierat <kasia.gierat@gmail.com>
2020-11-13 17:49:46 -0500

    bash, pwd, ls

--- bash.js
+++ bash.js
@@ -1,1 +1,20 @@
-console.log('hi');
+// const fs = require('fs');
+
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+});
+
+
+const pwd = require('./pwd');
+const ls = require('./ls');
+
+pwd()
+ls()
+
+
